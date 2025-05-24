9596a23ecce899fe122cd55a5c0c40d44782c5fd
Tausif <tausif00100@gmail.com>
2025-05-24 16:33:40 +0530

    add conditional rendering

--- src/app/(protected)/(flashcard)/flashcard-screen.tsx
+++ src/app/(protected)/(flashcard)/flashcard-screen.tsx
@@ -1,34 +1,58 @@
-
-import { View, Text, Button } from 'react-native'
-import {useState} from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text, Button } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useLocalSearchParams } from 'expo-router';
 
 const FlashcardScreen = () => {
-  const {courseData} = useLocalSearchParams()
-  const [currentQna, setCurrentQna] = useState(0)
-    // console.log("all the data",data);
-    // so we got each courseData 
-    const parsedPracticeData = JSON.parse(courseData) 
-    const flashcard = parsedPracticeData?.flashcards[0]?.front
-    console.log("QNA :",flashcard);
-    
-    const onCompeletingQna = () => {
-      setCurrentQna(0+1)
-    }
-    
-    // console.log("CourseData from qna-screen :",JSON.stringify(parsedPracticeData,null, 4));
+  const { courseData } = useLocalSearchParams();
+  const [currentFlashcard, setCurrentFlashcard] = useState(0);
+  // console.log("all the data",data);
+  // so we got each courseData
+  const parsedPracticeData = JSON.parse(courseData);
+  const flashcard = parsedPracticeData?.flashcards;
+  console.log('falshCard :', flashcard);
+
+  const onCompeletingFlashcards = () => {
+    setCurrentFlashcard(0 + 1);
+  };
+  console.log('helo :', parsedPracticeData?.flashcards[0]?.front);
+
+  // console.log("CourseData from flashcard-screen :",JSON.stringify(parsedPracticeData,null, 4));
+  //  parsedPracticeData?.flashcards[0]?.front === null||undefined ?console.log("hello"):console.log("hnothing");
+  console.log("undefined :",parsedPracticeData?.flashcards.length);
+  
+   
+   
   return (
     <SafeAreaView>
       <Text>QnaScreen</Text>
-      {/* <Text>{parsedPracticeData.chaptersQna[currentQna].question}</Text> */}
-      {/* <Text>{parsedPracticeData.chaptersQna[currentQna].answer}</Text> */}
-      <Button 
-      title='NEXT'
-      onPress={() => onCompeletingQna()}
+      {
+        // parsedPracticeData?.flashcards[0]?.front !== null||undefined ? (
+        parsedPracticeData?.flashcards.length >0 ? (
+          <View>
+  <Text>{parsedPracticeData?.flashcards[currentFlashcard]?.front}</Text>
+      <Text>{parsedPracticeData?.flashcards[currentFlashcard]?.back}</Text>
+          </View>
+        ): (<Text className=' text-xl'>THere are no flashcards</Text>)
+      }
+
+      {/* if there are enough cards then show otherwise don't move */}
+      
+      {/* 
+      {
+    
+      } */}
+      <Button
+        title="NEXT"
+        onPress={() => {
+          if (parsedPracticeData?.flashcards.length < currentFlashcard - 1) {
+            console.log('hello');
+          }
+          onCompeletingFlashcards();
+        }}
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default FlashcardScreen
+export default FlashcardScreen;
